4070ac36257bf6117ffcc77e04bab35859e95505
Elias Roussos <eliasisaiah@outlook.com>
2023-12-18 18:22:36 -0500

    wds mongo follow-along

--- activities/week-13-wds-mongo-mongoose/script.js
+++ activities/week-13-wds-mongo-mongoose/script.js
@@ -1,17 +1,19 @@
 const mongoose = require("mongoose");
 const User = require("./User");
 
-const uri = "";
+const uri = "mongodb+srv://cclUser:superSecurePassword@colorcodedlabs.jnwokit.mongodb.net/?retryWrites=true&w=majority";
 mongoose.connect(uri);
 
 run();
 async function run() {
-    const userSierra = new User({ name: "Sierra", age: 26 })
-    await userSierra.save();
-    console.log(userSierra);
+    const rubyUser = await User.create({
+        name: "Ruby",
+        age: 2,
+        hobbies: ["long walks on the beach", "drinking from the skulls of my enemies"],
+        address: {
+            street: "90210 Hollywood Ave"
+        }
+    });
 
-    const userChiron = await User.create({name: "Chiron", age: 9})
-    userChiron.age = 9001;
-    await userChiron.save();
-    console.log(userChiron);
+    console.log(rubyUser);
 }
